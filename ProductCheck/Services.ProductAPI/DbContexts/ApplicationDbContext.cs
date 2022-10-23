c7523fe30957b71998d7b6f54af08992e82f732c
unknown <daniel.macri87@gmail.com>
2022-10-23 05:20:37 -0300

    Seeder

--- ProductCheck/Services.ProductAPI/DbContexts/ApplicationDbContext.cs
+++ ProductCheck/Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -11,5 +11,81 @@ namespace Services.ProductAPI.DbContexts
 
         public DbSet<Product> DMProductsAPI { get; set; }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductID = 1,
+                ProductName = "Chai",
+                SupplierID = 1,
+                CategoryID = 1,
+                QuantityPerUnit = "10 boxes x 20 bags",
+                UnitPrice = 18,
+                UnitsInStock = 39,
+                UnitsOnOrder = 0,
+                ReorderLevel = 10,
+                Discontinued = false
+
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductID = 2,
+                ProductName = "Chang",
+                SupplierID = 1,
+                CategoryID = 1,
+                QuantityPerUnit = "24 - 12 oz bottles",
+                UnitPrice = 19,
+                UnitsInStock = 17,
+                UnitsOnOrder = 40,
+                ReorderLevel = 25,
+                Discontinued = false
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+
+                ProductID = 3,
+                ProductName = "Aniseed Syrup",
+                SupplierID = 1,
+                CategoryID = 2,
+                QuantityPerUnit = "12 - 550 ml bottles",
+                UnitPrice = 10,
+                UnitsInStock = 13,
+                UnitsOnOrder = 70,
+                ReorderLevel = 25,
+                Discontinued = false
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductID = 4,
+                ProductName = "Chef Anton's Cajun Seasoning",
+                SupplierID = 2,
+                CategoryID = 2,
+                QuantityPerUnit = "48 - 6 oz jars",
+                UnitPrice = 22,
+                UnitsInStock = 53,
+                UnitsOnOrder = 0,
+                ReorderLevel = 0,
+                Discontinued = false
+            });
+
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                ProductID = 5,               
+                ProductName = "Chef Anton's Gumbo Mix",
+                SupplierID = 2,
+                CategoryID = 2,
+                QuantityPerUnit = "36 boxes",
+                UnitPrice = 21,
+                UnitsInStock = 0,
+                UnitsOnOrder = 0,
+                ReorderLevel = 0,
+                Discontinued = true
+            });
+        }
+
     }
 }
---
+++ ProductCheck/Services.ProductAPI/Migrations/20221023081930_seed-product.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Services.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedproduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "QuantityPerUnit",
+                table: "DMProductsAPI",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.InsertData(
+                table: "DMProductsAPI",
+                columns: new[] { "ProductID", "CategoryID", "Discontinued", "ProductName", "QuantityPerUnit", "ReorderLevel", "SupplierID", "UnitPrice", "UnitsInStock", "UnitsOnOrder" },
+                values: new object[,]
+                {
+                    { 1, 1, false, "Chai", "10 boxes x 20 bags", (short)10, 1, 18m, (short)39, (short)0 },
+                    { 2, 1, false, "Chang", "24 - 12 oz bottles", (short)25, 1, 19m, (short)17, (short)40 },
+                    { 3, 2, false, "Aniseed Syrup", "12 - 550 ml bottles", (short)25, 1, 10m, (short)13, (short)70 },
+                    { 4, 2, false, "Chef Anton's Cajun Seasoning", "48 - 6 oz jars", (short)0, 2, 22m, (short)53, (short)0 },
+                    { 5, 2, true, "Chef Anton's Gumbo Mix", "36 boxes", (short)0, 2, 21m, (short)0, (short)0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DMProductsAPI",
+                keyColumn: "ProductID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DMProductsAPI",
+                keyColumn: "ProductID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DMProductsAPI",
+                keyColumn: "ProductID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "DMProductsAPI",
+                keyColumn: "ProductID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "DMProductsAPI",
+                keyColumn: "ProductID",
+                keyValue: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuantityPerUnit",
+                table: "DMProductsAPI",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
